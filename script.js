b7e62b8c308a86e08ea4709c0369d95cc250b074
Rostik Yuriev <rost4402255@gmail.com>
2017-11-02 19:33:18 +0200

    redone js

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-var i = 0;
+var flag = false;
 
-while (i == 0) {
-  var ask = prompt("введите месяц своего рождения" , "01");
-  if(ask != ""){
-    i = 1;
-  }
-}
+var ask;
 
-switch (ask) {
-  case "июнь":
-  case "июль":
-  case "август":
-  case "06":
-  case "07":
-  case "08":
-    alert("вы родились летом");
-    break
-  case "сентябрь":
-  case "октябрь":
-  case "ноябрь":
-  case "09":
-  case "10":
-  case "11":
-    alert("вы родились осенью");
-    break;
-  case "декабрь":
-  case "январь":
-  case "февраль":
-  case "12":
-  case "01":
-  case "02":
-    alert("вы родились зимой");
-    break;
-  case "март":
-  case "апрель":
-  case "май":
-  case "03":
-  case "04":
-  case "05":
-    alert("вы родились весной");
-    break;
-  default:
-    alert("нет такого месяца");
+while (flag == false) {
+  ask = prompt("введите месяц своего рождения" , "1");
+    switch (ask) {
+      case "июнь":
+      case "июль":
+      case "август":
+      case "6":
+      case "7":
+      case "8":
+        alert("вы родились летом");
+        flag = true;
+        break;
+      case "сентябрь":
+      case "октябрь":
+      case "ноябрь":
+      case "9":
+      case "10":
+      case "11":
+        alert("вы родились осенью");
+        flag = true;
+        break;
+      case "декабрь":
+      case "январь":
+      case "февраль":
+      case "12":
+      case "1":
+      case "2":
+        alert("вы родились зимой");
+        flag = true;
+        break;
+      case "март":
+      case "апрель":
+      case "май":
+      case "3":
+      case "4":
+      case "5":
+        alert("вы родились весной");
+        flag = true;
+        break;
+      case null:
+        flag = true;
+        break;
+      default:
+        alert("нет такого месяца");
+    }
 }
